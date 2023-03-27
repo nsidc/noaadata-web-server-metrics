cad26667484d5b8fd6499c9c0a6d572c70ca51d7
Robyn Marowitz <robyn.marowitz@colorado.edu>
2023-03-27 20:24:12 +0000

    Bring over updated cli from old project - includes dataset parameter

--- noaa_metrics/cli.py
+++ noaa_metrics/cli.py
@@ -2,30 +2,31 @@ import datetime as dt
 
 import click
 
-# class DateType(click.ParamType):
-#     name = 'date'
-#
-#     def __init__(self, formats=None):
-#         self.formats = formats or [
-#             '%Y-%m-%d',
-#             '%Y%m%d',
-#         ]
-#
-#     def __repr__(self):
-#         return 'Date'
-#
-#     def get_metavar(self, param):
-#         formats_str = "|".join(self.formats)
-#         return f'[{formats_str}]'
-#
-#     def convert(self, value, param, ctx):
-#         for fmt in self.formats:
-#             try:
-#                 return dt.datetime.strptime(value, fmt).date()
-#             except ValueError:
-#                 continue
-#
-#         self.fail(f'{value} is not a valid date. Expected one of: {self.formats}')
+
+class DateType(click.ParamType):
+    name = "date"
+
+    def __init__(self, formats=None):
+        self.formats = formats or [
+            "%Y-%m-%d",
+            "%Y%m%d",
+        ]
+
+    def __repr__(self):
+        return "Date"
+
+    def get_metavar(self, param):
+        formats_str = "|".join(self.formats)
+        return f"[{formats_str}]"
+
+    def convert(self, value, param, ctx):
+        for fmt in self.formats:
+            try:
+                return dt.datetime.strptime(value, fmt).date()
+            except ValueError:
+                continue
+
+        self.fail(f"{value} is not a valid date. Expected one of: {self.formats}")
 
 
 @click.group()
@@ -36,24 +37,31 @@ def cli():
 @cli.command(
     short_help="Generate NOAA downlaods metric report.",
 )
+# TODO: let click make it a datetime then turn it back to a string (isoformat) (string function)
 @click.option(
     "-s",
     "--start_date",
     help="Start date (YYYY-MM-DD)",
-    # type=DateType(),
+    type=DateType(),
 )
 @click.option(
     "-e",
     "--end_date",
     help="End date (YYYY-MM-DD)",
-    # type=DateType(),
+    type=DateType(),
 )
 @click.option("-m", "--mailto", help="Email(s) to send report to.", multiple=True)
-def process(start_date, end_date, mailto):
+@click.option(
+    "-d",
+    "--dataset",
+    help="Select a specific dataset or all of them(default).",
+    default="all",
+)
+def process(start_date, end_date, mailto, dataset):
     """Generate NOAA downlaods metric report."""
     from aggregate_logs import main
 
-    main(start_date=start_date, end_date=end_date, mailto=mailto)
+    main(start_date=start_date, end_date=end_date, mailto=mailto, dataset=dataset)
 
 
 if __name__ == "__main__":
