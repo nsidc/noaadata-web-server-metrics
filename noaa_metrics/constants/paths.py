0a6344b2f7dc90a4bf539c794a9dc506a684b466
Robyn Marowitz <robyn.marowitz@colorado.edu>
2023-03-30 23:46:36 +0000

    Notes on paths

--- noaa_metrics/constants/paths.py
+++ noaa_metrics/constants/paths.py
@@ -1,11 +1,14 @@
 from pathlib import Path
 
+# Where we get the logs
 LOG_DIR = Path("/share/logs/noaa-web")
 NGINX_DOWNLOAD_LOG_FILE = LOG_DIR / "download.log"
 
 PACKAGE_DIR = Path(__file__).parent.parent
 PROJECT_DIR = PACKAGE_DIR.parent
 
+# Log output locations
+# TODO: update from tmp to share/logs potentially
 JSON_OUTPUT_DIR = Path("/tmp")
 REPORT_OUTPUT_DIR = Path("/tmp")
 REPORT_OUTPUT_FILEPATH = REPORT_OUTPUT_DIR / "noaa-downloads.csv"
