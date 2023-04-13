48551d2ddeffb10a6c2893cea96864d23fdb6088
Robyn Marowitz <robyn.marowitz@colorado.edu>
2023-04-13 19:44:54 +0000

    resolve def error

--- noaa_metrics/util/json.py
+++ noaa_metrics/util/json.py
@@ -3,7 +3,7 @@ import json
 from typing import Any
 
 class DateFriendlyJSONEncoder(json.JSONEncoder):
-    def default(self: DateFriendlyJSONEncoder, obj: Any) -> Any:
+    def default(self, obj: Any) -> Any:
         if isinstance(obj, dt.date):
             return obj.isoformat()
         return super(DateFriendlyJSONEncoder, self).default(obj)
