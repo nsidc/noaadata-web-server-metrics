0817ab01788aeab595f2bd7b3dbfaae62ed24120
Robyn Marowitz <robyn.marowitz@colorado.edu>
2023-04-18 17:38:16 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="noaadata-web-server-metrics",
+    version="version="0.1.0",",
+    description="Generate email report of NSIDC NOAA https download metrics.",
+    url="git@github.com:nsidc/noaadata-web-server-metrics.git",
+    author="National Snow and Ice Data Center",
+    author_email="nsidc@nsidc.org",
+    packages=find_packages(),
+    entry_points={"console_scripts": ["noaa_metrics = noaa_metrics.cli:cli"]},
+    include_package_data=True,
+)
